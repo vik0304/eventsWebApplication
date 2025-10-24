e2a74f5c848068787e76ef7e8f8ca075a8e7b2f4
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-10-24 10:47:53 +0200

    adds user entity and repository

---
+++ src/main/java/viktor/vasileski/eventsWebApplication/entities/User.java
@@ -0,0 +1,44 @@
+package viktor.vasileski.eventsWebApplication.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_type")
+    private UserType userType;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    @ManyToMany(mappedBy = "participants")
+    private Set<Event> reservedEvent = new HashSet<>();
+
+    public User(UserType userType, String name, String surname, String email, String password){
+        this.userType=userType;
+        this.name=name;
+        this.surname=surname;
+        this.email=email;
+        this.password=password;
+    }
+
+    public void addReservation(Event event){
+        reservedEvent.add(event);
+        event.getParticipants().add(this);
+    }
+}
---
+++ src/main/java/viktor/vasileski/eventsWebApplication/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package viktor.vasileski.eventsWebApplication.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import viktor.vasileski.eventsWebApplication.entities.User;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
