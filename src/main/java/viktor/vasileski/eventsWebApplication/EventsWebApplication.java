84669ebd617a6e2175f43cb5f8cdc4ec9f0c882b
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-10-24 09:13:26 +0200

    Initial commit

---
+++ src/main/java/viktor/vasileski/eventsWebApplication/EventsWebApplication.java
@@ -0,0 +1,13 @@
+package viktor.vasileski.eventsWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/viktor/vasileski/eventsWebApplication/EventsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package viktor.vasileski.eventsWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
