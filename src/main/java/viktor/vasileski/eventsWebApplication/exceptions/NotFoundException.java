9dce6f084f03dd75f127bea854d53061460bbbf5
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-10-24 12:45:18 +0200

    updates not found exception

--- src/main/java/viktor/vasileski/eventsWebApplication/exceptions/NotFoundException.java
+++ src/main/java/viktor/vasileski/eventsWebApplication/exceptions/NotFoundException.java
@@ -1,7 +1,13 @@
 package viktor.vasileski.eventsWebApplication.exceptions;
 
+import java.util.UUID;
+
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
+    public NotFoundException(UUID id) {
+        super("Il record con id " + id + " non è stato trovato!");
+    }
+
+    public NotFoundException(String msg) {
+        super(msg);
     }
 }
